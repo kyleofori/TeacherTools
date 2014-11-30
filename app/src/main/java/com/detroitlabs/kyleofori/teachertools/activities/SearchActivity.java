c8bf8144092748bcc522a11d2e182e0907361728
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-11-30 08:17:51 -0500

    Connecting click of button to trySearchTermSubmit method

--- app/src/main/java/com/detroitlabs/kyleofori/teachertools/activities/SearchActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/teachertools/activities/SearchActivity.java
@@ -1,20 +1,30 @@
 package com.detroitlabs.kyleofori.teachertools.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.detroitlabs.kyleofori.teachertools.R;
 
 /**
  * Created by kyleofori on 11/30/14.
  */
-public class SearchActivity extends Activity {
+public class SearchActivity extends Activity implements View.OnClickListener {
+
+    private EditText edtSearchBar;
+    private Button btnSearch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        edtSearchBar = (EditText) findViewById(R.id.edt_search_bar);
+        btnSearch = (Button) findViewById(R.id.btn_search);
     }
 
     @Override
@@ -25,5 +35,18 @@ public class SearchActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_search:
+                trySearchTermSubmit();
+                break;
+        }
+    }
+
+    public void trySearchTermSubmit() {
+        
     }
 }
