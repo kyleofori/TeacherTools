d606d02ef673249d065b711397775db2281c7a1f
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-11-30 08:03:53 -0500

    Creating SearchActivity

---
+++ app/src/main/java/com/detroitlabs/kyleofori/teachertools/activities/SearchActivity.java
@@ -0,0 +1,26 @@
+package com.detroitlabs.kyleofori.teachertools.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by kyleofori on 11/30/14.
+ */
+public class SearchActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
