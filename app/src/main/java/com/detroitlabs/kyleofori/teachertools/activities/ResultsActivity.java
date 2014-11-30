2cfcfd07ff6864be6c98ba06f99d5330f7efd003
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-11-30 09:07:53 -0500

    Connecting SearchActivity to ResultsActivity

--- app/src/main/java/com/detroitlabs/kyleofori/teachertools/activities/ResultsActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/teachertools/activities/ResultsActivity.java
@@ -1,7 +1,18 @@
 package com.detroitlabs.kyleofori.teachertools.activities;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.detroitlabs.kyleofori.teachertools.R;
+
 /**
  * Created by kyleofori on 11/30/14.
  */
-public class ResultsActivity {
+public class ResultsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
 }
