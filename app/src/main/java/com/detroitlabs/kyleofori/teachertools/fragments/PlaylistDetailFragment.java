592830b1c0adde8ecaff0fe31f888e13093fb93e
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-12-03 11:44:17 -0500

    Changing names of TextViews in PlaylistDetailFragment

--- app/src/main/java/com/detroitlabs/kyleofori/teachertools/fragments/PlaylistDetailFragment.java
+++ app/src/main/java/com/detroitlabs/kyleofori/teachertools/fragments/PlaylistDetailFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.detroitlabs.kyleofori.teachertools.R;
@@ -28,15 +29,15 @@ public class PlaylistDetailFragment extends Fragment {
         return playlistDetailFragment;
     }
 
-    private TextView titleTextView, kaUrlTextView, descriptionTextView;
+    private TextView txtTitle, txtKaUrl, txtDescription;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_playlist_detail, container, false);
 
-        titleTextView = (TextView) view.findViewById(R.id.titleTextView);
-        kaUrlTextView = (TextView) view.findViewById(R.id.kaUrlTextView);
-        descriptionTextView = (TextView) view.findViewById(R.id.descriptionTextView);
+        txtTitle = (TextView) view.findViewById(R.id.titleTextView);
+        txtKaUrl = (TextView) view.findViewById(R.id.kaUrlTextView);
+        txtDescription = (TextView) view.findViewById(R.id.descriptionTextView);
 
         return view;
     }
@@ -49,9 +50,9 @@ public class PlaylistDetailFragment extends Fragment {
 
         if (khanAcademyPlaylist != null) {
 
-            titleTextView.setText(khanAcademyPlaylist.getTitle());
-            kaUrlTextView.setText(khanAcademyPlaylist.getKaUrl());
-            descriptionTextView.setText(khanAcademyPlaylist.getDescription());
+            txtTitle.setText(khanAcademyPlaylist.getTitle());
+            txtKaUrl.setText(khanAcademyPlaylist.getKaUrl());
+            txtDescription.setText(khanAcademyPlaylist.getDescription());
 
         } else {
             throw new IllegalStateException("Must supply a KhanAcademyPlaylist to PlaylistDetailFragment");
