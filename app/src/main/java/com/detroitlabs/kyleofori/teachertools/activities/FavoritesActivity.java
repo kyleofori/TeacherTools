102b5c667c98cc71f81792e4756d0ce494e73ed0
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-12-07 19:14:21 -0500

    Creating intent from ResultsActivity to FavoritesActivity--also just created

---
+++ app/src/main/java/com/detroitlabs/kyleofori/teachertools/activities/FavoritesActivity.java
@@ -0,0 +1,9 @@
+package com.detroitlabs.kyleofori.teachertools.activities;
+
+import android.app.Activity;
+
+/**
+ * Created by kyleofori on 12/7/14.
+ */
+public class FavoritesActivity extends Activity {
+}
--- app/src/main/java/com/detroitlabs/kyleofori/teachertools/activities/ResultsActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/teachertools/activities/ResultsActivity.java
@@ -3,7 +3,10 @@ package com.detroitlabs.kyleofori.teachertools.activities;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.detroitlabs.kyleofori.teachertools.R;
 import com.detroitlabs.kyleofori.teachertools.fragments.SearchResultsFragment;
@@ -13,9 +16,11 @@ import com.parse.Parse;
 /**
  * Created by kyleofori on 11/30/14.
  */
-public class ResultsActivity extends Activity implements FragmentController {
+public class ResultsActivity extends Activity implements FragmentController, View.OnClickListener {
 
     public static final String EXTRA_SEARCH_KEYWORD = "search keyword";
+
+    private Button btnSeeFavorites;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,19 @@ public class ResultsActivity extends Activity implements FragmentController {
         Parse.initialize(this, "t7JTeQx4arA61XvUhQa1CjJhA2VrKWv9mMj3p44G", "jzKFoFbkf2FYwMf9sNwIegGFPt5oNBWwbsffNqa3");
 
         setContentView(R.layout.activity_results);
+        btnSeeFavorites = (Button) findViewById(R.id.btn_see_favorites);
+        btnSeeFavorites.setOnClickListener(this);
         loadSearchResultsFragment();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_see_favorites:
+                Intent intent = new Intent(this, FavoritesActivity.class);
+                startActivity(intent);
+        }
+
     }
 
     @Override
@@ -62,6 +79,4 @@ public class ResultsActivity extends Activity implements FragmentController {
             throw new IllegalStateException("Must supply a search term to ResultsActivity");
         }
     }
-
-
 }
