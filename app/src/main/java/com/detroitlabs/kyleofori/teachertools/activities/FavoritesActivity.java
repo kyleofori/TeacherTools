73aa2848c3fe5284ed12f063c52ab7e52e0dee57
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-12-07 20:16:50 -0500

    filling out FavoritesActivity

--- app/src/main/java/com/detroitlabs/kyleofori/teachertools/activities/FavoritesActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/teachertools/activities/FavoritesActivity.java
@@ -1,9 +1,47 @@
 package com.detroitlabs.kyleofori.teachertools.activities;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+
+import com.detroitlabs.kyleofori.teachertools.R;
+import com.detroitlabs.kyleofori.teachertools.interfaces.FragmentController;
 
 /**
  * Created by kyleofori on 12/7/14.
  */
-public class FavoritesActivity extends Activity {
+public class FavoritesActivity extends Activity implements FragmentController, View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() != 0) {
+            getFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void changeFragment(Fragment fragment, boolean addToBackStack) {
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+
+        if (addToBackStack) {
+            fragmentTransaction.addToBackStack(null);
+        }
+
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
