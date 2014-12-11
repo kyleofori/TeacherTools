6b48dec5fc4e33130466b5fb41fd3755f7847e03
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-12-11 17:36:30 -0500

    Adding a go to search button on the homepage

--- app/src/main/java/com/detroitlabs/kyleofori/teachertools/activities/HomepageActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/teachertools/activities/HomepageActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.detroitlabs.kyleofori.teachertools.R;
@@ -12,7 +14,6 @@ import com.detroitlabs.kyleofori.teachertools.khanacademyapi.KhanAcademyApi;
 import com.detroitlabs.kyleofori.teachertools.khanacademyapi.KhanAcademyApiCallback;
 import com.detroitlabs.kyleofori.teachertools.models.LessonModel;
 import com.detroitlabs.kyleofori.teachertools.parsers.KhanAcademyJSONParser;
-import com.detroitlabs.kyleofori.teachertools.tags.GlobalTags;
 import com.parse.Parse;
 
 import org.json.JSONArray;
@@ -22,10 +23,14 @@ import java.util.List;
 /**
  * Created by kyleofori on 11/30/14.
  */
-public class HomepageActivity extends Activity implements KhanAcademyApiCallback {
+public class HomepageActivity extends Activity implements KhanAcademyApiCallback, View.OnClickListener {
+
+    public static List<LessonModel> khanAcademyLessonModels;
 
     private KhanAcademyApi khanAcademyApi = KhanAcademyApi.getKhanAcademyApi();
-    public static List<LessonModel> khanAcademyLessonModels;
+    private Button btnGoToSearch;
+
+
 
 
     @Override
@@ -35,6 +40,9 @@ public class HomepageActivity extends Activity implements KhanAcademyApiCallback
         Parse.initialize(this, "t7JTeQx4arA61XvUhQa1CjJhA2VrKWv9mMj3p44G", "jzKFoFbkf2FYwMf9sNwIegGFPt5oNBWwbsffNqa3");
         setContentView(R.layout.activity_homepage);
         khanAcademyApi.getKhanAcademyPlaylists(this);
+        btnGoToSearch = (Button) findViewById(R.id.btn_go_to_search);
+        btnGoToSearch.setEnabled(false);
+        btnGoToSearch.setOnClickListener(this);
     }
 
     @Override
@@ -50,18 +58,28 @@ public class HomepageActivity extends Activity implements KhanAcademyApiCallback
     @Override
     public void onSuccess(JSONArray response) {
         khanAcademyLessonModels = KhanAcademyJSONParser.parseJSONObject(response);
-        openSearchResultsListFragment();
+        btnGoToSearch.setEnabled(true);
     }
 
     @Override
     public void onError() {
         Toast.makeText(this, "There was an error loading lesson plans from Khan Academy.", Toast.LENGTH_SHORT).show();
-        openSearchResultsListFragment();
+        btnGoToSearch.setEnabled(true);
+        btnGoToSearch.setText(getString(R.string.search_on_error));
     }
 
     public void openSearchResultsListFragment() {
         Intent toResultsActivity = new Intent(this, ResultsActivity.class);
         startActivity(toResultsActivity);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.btn_go_to_search:
+                openSearchResultsListFragment();
+                break;
+        }
     }
 }
 
